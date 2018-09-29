7e96795e090f2715a12907f008e73099af240bf1
cybadrian <cybadrian@gmail.com>
2018-09-29 14:51:10 +0200

    Zadanie nr 2

--- js/app.js
+++ js/app.js
@@ -1,14 +1,36 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
-    var menuButton = document.querySelector(' .for-dropdown');
-    var subMenu = document.querySelector(' .dropdown');
+var menuButton = document.querySelector('.for-dropdown');
+var subMenu = document.querySelector('.dropdown');
     
-    menuButton.addEventListener('mouseover', function () {
-      subMenu.style.display = 'block';
+    menuButton.addEventListener('mouseover', function() {
+        subMenu.style.display = 'block';
      });
+ 
+    menuButton.addEventListener('mouseout', function() {
+        subMenu.style.display = 'none';
+      });
     
-    menuButton.addEventListener('mouseout', function () {
-      subMenu.style.display = 'none';
-    });  
+    //zadanie 2 
+    
+    
+    function showHide(){
+                var textArea = this.previousElementSibling;
+        
+        if (textArea.style.display === 'none' || textArea.style.display === '') {
+          textArea.style.display = 'block';
+            this.innerHTML = 'MNIEJ <span class="glyphicon glyphicon-chevron-up"></span>';
+        } else {            
+        textArea.style.display = 'none';
+        this.innerHTML = 'WIĘCEJ <span class="glyphicon glyphicon-chevron-down"></span>';
+        }
+    }
+    
+    var buttons = document.querySelectorAll('.read-more');
+    
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', showHide);
+    }
+
 });
